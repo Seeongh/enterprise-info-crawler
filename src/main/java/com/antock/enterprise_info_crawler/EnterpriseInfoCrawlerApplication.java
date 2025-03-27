12d112dbabf77a92571625eee6bee22d95844741
Seeongh <mikstic7@gmail.com>
2025-03-27 13:18:56 +0900

    feat: Spring boot 환경 설정

---
+++ src/main/java/com/antock/enterprise_info_crawler/EnterpriseInfoCrawlerApplication.java
@@ -0,0 +1,14 @@
+package com.antock.enterprise_info_crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseInfoCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseInfoCrawlerApplication.class, args);
+	}
+
+}
+zpav;ld
