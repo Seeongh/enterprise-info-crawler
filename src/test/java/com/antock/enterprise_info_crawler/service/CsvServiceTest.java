cd126ed1bda34e409392274f2f91dd412702dcfe
Seeongh <mikstic7@gmail.com>
2025-03-27 21:49:27 +0900

    test: csv reader 테스트 코드 입력

--- src/test/java/com/antock/enterprise_info_crawler/service/CsvServiceTest.java
+++ src/test/java/com/antock/enterprise_info_crawler/service/CsvServiceTest.java
@@ -1,2 +1,57 @@
-package com.antock.enterprise_info_crawler.service;public class CsvServiceTest {
+package com.antock.enterprise_info_crawler.service;
+
+import com.antock.enterprise_info_crawler.api.coseller.application.CsvService;
+import com.antock.enterprise_info_crawler.api.coseller.application.dto.BizCsvInfo;
+import com.antock.enterprise_info_crawler.api.coseller.value.City;
+import com.antock.enterprise_info_crawler.api.coseller.value.District;
+import com.antock.enterprise_info_crawler.common.constants.CsvConstants;
+import org.assertj.core.api.Assertions;
+import org.hibernate.annotations.CurrentTimestamp;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class CsvServiceTest {
+
+    @Autowired
+    private CsvService csvService;
+
+    private City city;
+    private District district;
+    @BeforeEach
+    void setUp() {
+        city = City.valueOf("서울특별시");
+        district = District.valueOf("강남구");
+
+    }
+
+    @Test
+    @DisplayName("csv 파일 조회 검증")
+    public void read_csv_file_test() throws Exception {
+        //given
+
+        //when
+         long startTime =  System.currentTimeMillis();
+        List<BizCsvInfo> list = csvService.readBizCsv(city.name(), district.name());
+
+        long endTime =  System.currentTimeMillis();
+        System.out.println("소요시간 = " + (endTime - startTime) + " ms");
+        //then
+        assertNotNull(list);
+
+        BizCsvInfo info = list.get(0);
+        Assertions.assertThat(CsvConstants.CORP_TYPE_BIZ).isEqualTo(info.getBizType());
+     }
 }
