6e8e5166cf1b127caaf0fc495a9ba7c385d7b4a8
Seeongh <mikstic7@gmail.com>
2025-03-27 14:51:20 +0900

    feat: 공통 응답 class 생성

--- src/main/java/com/antock/enterprise_info_crawler/common/response/ApiResponse.java
+++ src/main/java/com/antock/enterprise_info_crawler/common/response/ApiResponse.java
@@ -1,2 +1,41 @@
-package com.antock.enterprise_info_crawler.common.response;public class ApiResponse {
+package com.antock.enterprise_info_crawler.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+/**
+ * API응답 공통 처리
+ * @param <T>
+ */
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private int resultCode;
+    private String resultMsg;
+    private T data; //응답데이터
+
+    public ApiResponse(HttpStatus resultStatus, String resultMsg) {
+        this.resultCode = resultStatus.value();
+        this.resultMsg = resultMsg;
+    }
+
+    public ApiResponse(HttpStatus resultStatus, String resultMsg, T data) {
+        this.resultCode = resultStatus.value();
+        this.resultMsg = resultMsg;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus resultStatus, T data) {
+        return new ApiResponse<>(resultStatus, resultStatus.getReasonPhrase(), data);
+    }
+
+
+
 }
